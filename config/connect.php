60d2061a5e10891b0b9ab93f7223a824df84c961
Khachonsak22 <67010974002@msu.ac.th>
2026-03-06 01:11:54 +0700

    AAA

--- config/connect.php
+++ config/connect.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 mysqli_query($conn,"SET NAMES utf8");
 
 // ประกาศตัวแปรค่าคงที่สำหรับ Gemini API Key
-define('GEMINI_API_KEY', 'AIzaSyCOYyxP0y_3_8bz_hZexiBGzQRvW-XRViw');
+define('GEMINI_API_KEY', 'AIzaSyCWlEusZKBN4hAMZUn5JFTrMEdxinFQ8r8');
 define('N8N_SECRET', 'f8a3c9b2-1e4d-4a7f-9c2b-8d6e5a4f3b1c');
 
 ?>
