5c6586ed8b83ed4c99e1ab2af32ea7f4766f1d3e
Khachonsak22 <67010974002@msu.ac.th>
2026-03-23 23:11:27 +0700

    201

--- index.php
+++ index.php
@@ -235,7 +235,7 @@ body{font-family:'Kanit',sans-serif;overflow-x:hidden;background:#fff;}
 }
 .food-card img{
   width:100%;
-  height:250px;
+  height:300px;
   object-fit:cover;
   display:block;
 }
